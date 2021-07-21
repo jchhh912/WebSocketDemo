17b20f5a212fbd38f3472502a473f58addd6ed6f
Jiang <54928439+jchhh912@users.noreply.github.com>
2021-07-21 17:09:45 +0800

    Update WebSocketMessageHandler.cs
    重写方法

--- WebSocketDemo/Sockets/WebSocketMessageHandler.cs
+++ WebSocketDemo/Sockets/WebSocketMessageHandler.cs
@@ -1,4 +1,4 @@
-﻿
+
 
 using Newtonsoft.Json;
 using System.Net.WebSockets;
@@ -32,12 +32,44 @@ namespace WebSocketDemo
             await base.OnDisconnected(socket);
             await SendMessageToAll($"{socketId}离开了");
         }
-        //处理连接
+        //正常数据处理
         public override async Task Receive(WebSocket socket, WebSocketReceiveResult result, byte[] buffer)
         {
 
             var msgString = Encoding.UTF8.GetString(buffer, 0, result.Count);
             var msg = JsonConvert.DeserializeObject<Message>(msgString);
+            msg.FromID = Sockets.GetId(socket);
+            switch (msg.Action)
+            {
+                case "join":
+                    //重新连接
+                    await OnConnected(socket);
+                    break;
+                case "leave":
+                    //断开
+                    await OnDisconnected(socket);
+                    break;
+                case "Public":
+                    //公开消息
+                    await SendMessageToAll($"{msg.Name} 发送了公共消息：{msg.Msg}");
+                    break;
+                case "private":
+                    //私聊消息
+                    await SendMessage(msg.ToID, $"来自{msg.FromID}的消息:{msg.Msg}");
+                    break;
+                case "info":
+                    //查询信息
+                    break;
+
+                default:
+                    break;
+            }
+        }
+        //传输大数据
+        public override async Task Receive(WebSocket socket, string result)
+        {
+
+            Message msg = JsonConvert.DeserializeObject<Message>(result);
             msg.FromID = Sockets.GetId(socket);
             switch (msg.Action)
             {
