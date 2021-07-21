ab1df58bb1c990e46fb8ae8f17e8fbdc0e26cca3
Jiang <54928439+jchhh912@users.noreply.github.com>
2021-07-21 17:07:00 +0800

    Update SocketsMiddleware.cs
    切割帧块 来传输超长内容

--- WebSocketDemo/Sockets/SocketsMiddleware.cs
+++ WebSocketDemo/Sockets/SocketsMiddleware.cs
@@ -1,6 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Net.WebSockets;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -27,50 +28,65 @@ namespace WebSocketDemo
                 //转换为ws连接
                 var socket = await context.WebSockets.AcceptWebSocketAsync();
                 await _Handler.OnConnected(socket);
-                //接收消息
+                //接收消息 缓存区 需要合理设置,太小websocket接收缓存不足时，会自行断开后，过大会造成浪费大量内存
                 var buffer = new byte[1024 * 1];
                 var offset = 0;
-                var free = buffer.Length;
-                //判断连接
+                var free = buffer.Length;         
+                StringBuilder msgString = new StringBuilder();
+                //监听数据 
                 while (socket.State == WebSocketState.Open)
                 {
-                    var result = await socket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-                    offset += result.Count;
+                    //再次发送消息的时候 会将上一次的消息一起发过来
+                    WebSocketReceiveResult result = await socket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
                     free -= result.Count;
-                    if (result.EndOfMessage) break;
-                    if (free == 0)
+                   
+                    //当缓存池不足时,接收大文件
+                    if (free <= 0)
                     {
-                        // No free space
-                        // Resize the outgoing buffer
-                        var newSize = buffer.Length + 1024;
-                        // Check if the new size exceeds a limit
-                        // It should suit the data it receives
-                        // This limit however has a max value of 2 billion bytes (2 GB)
-                        if (newSize > 1024)
+                        
+                        offset += result.Count;
+                        // Resize the outgoing buffer  每次增加1024
+                        var newSize = buffer.Length + 1024 * 1;
+                        //设置缓存池最大限制  不超过8k-2.5mb 之间  计算方法https://stackoverflow.com/questions/2811006/what-is-a-good-buffer-size-for-socket-programming
+                        if (newSize > 262144)
                         {
-                            throw new Exception("Maximum size exceeded");
+                            //避免浪费 用户自己检查问题
+                            await _Handler.SendMessage(socket,"数据传输失败！请检查网络");
+                            break;
                         }
+                        //获取新缓存池
                         var newBuffer = new byte[newSize];
-                        Array.Copy(buffer, 0, newBuffer, 0, offset);
+                        Array.Copy(buffer, 0, newBuffer, 0, buffer.Length);
                         buffer = newBuffer;
                         free = buffer.Length - offset;
+                        //将接收到的数据一块一块切割保存
+                        msgString.Append(Encoding.UTF8.GetString(buffer, 0, result.Count));
+                        //是否完整接收数据 
+                        if (result.EndOfMessage && result.MessageType == WebSocketMessageType.Text)
+                        {
+                            await _Handler.Receive(socket, msgString.ToString());
+                            //释放
+                            msgString.Clear();
+                        };
                     }
-                    switch (result.MessageType)
-                    {
-                        case WebSocketMessageType.Text:
-                            await _Handler.Receive(socket, result, buffer);
-                            break;
-                        case WebSocketMessageType.Binary:
-                            break;
-                        case WebSocketMessageType.Close:
-                            await _Handler.OnDisconnected(socket);
-                            break;
-                        default:
-                            throw new AbandonedMutexException();
-                    }
-                }
+                    //当缓存池接收没问题时不用增加
+                    if (result.EndOfMessage&&free>0) {
+                        switch (result.MessageType)
+                        {
+                            case WebSocketMessageType.Text:
+                                await _Handler.Receive(socket, result, buffer);
+                                break;
+                            case WebSocketMessageType.Binary:
+                                break;
+                            case WebSocketMessageType.Close:
+                                await _Handler.OnDisconnected(socket);
+                                break;
+                            default:
+                                throw new AbandonedMutexException();
+                        }
+                    };
+                }    
             }
         }
-
     }
 }
